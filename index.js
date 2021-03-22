6ccb6307e004332b9148314ba019056e37ecc1a1
Jonathan Corley <jcorley@noscihigh.org>
2021-03-22 15:47:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+/*
+PROMPT ONE
+Declare a function and name it add
+
+This function will take two parameters called 
+num1 & num2.
+
+add should return the sum of these two parameters
+*/
+
+
+
+
+
+
+
+
+/*
+Use a function(PROMPT TWO)
+
+Declare a function called printer
+The printer function should print to the console no return or parameter needed
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+Using Arguments(PROMPT THREE)
+
+Declare a variable called myObject, and assign it the value 
+of an object
+
+Declare a function called addKeys
+
+This function should take one parameter called object
+
+This function will add two keys named firstName and lastName 
+to the given paramater
+
+The firstName key will be your first name as a string
+
+The lastName key will be your last name as a string
+
+This function will return the paramater
+
+Call the addKeys function outside its function definition and 
+pass myObject as an argument.
+*/
